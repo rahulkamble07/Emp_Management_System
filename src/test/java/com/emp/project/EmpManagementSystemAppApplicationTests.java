49f4cc81fc7f1d0fd9825f39257339e040ebe7b7
Rahul <rahulkamble191998@gmailcom>
2024-02-12 18:27:16 +0530

    initial commit

---
+++ src/test/java/com/emp/project/EmpManagementSystemAppApplicationTests.java
@@ -0,0 +1,13 @@
+package com.emp.project;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EmpManagementSystemAppApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
